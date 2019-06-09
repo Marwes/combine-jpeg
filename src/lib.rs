7fb8eec1075a593d7ad31425520ca0fcc4481c27
Markus Westerlind <marwes91@gmail.com>
2019-06-10 00:14:48 +0200

    Parse start of input markers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,37 @@
-type JPEG = ();
+use combine::{
+    parser::{byte::byte, item::satisfy_map},
+    ParseError, Parser, RangeStream,
+};
 
-pub fn decode(input: &[u8]) -> Result<JPEG, ()> {
-    Ok(())
+enum Marker {
+    SOI,
+    SOF(u8),
+    DHT,
+    DQT,
+    DRI,
+    SOS,
+    RST(u8),
+    APP(u8),
+    COM(u8),
+    EOI,
+}
+
+fn marker<'a, I>() -> impl Parser<Output = Marker, Input = I>
+where
+    I: RangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    byte(0xFF).with(satisfy_map(|b| {
+        Some(match b {
+            0xD8 => Marker::SOI,
+            _ => return None,
+        })
+    }))
+}
+
+pub fn decode(input: &[u8], output: &mut [u8]) -> Result<(), ()> {
+    let mut parser = marker();
+    parser.parse(input).map(|_| ()).map_err(|_| ())
 }
 
 #[cfg(test)]
@@ -10,6 +40,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert!(decode(include_bytes!("../img0.jpg")).is_ok());
+        assert!(decode(include_bytes!("../img0.jpg"), &mut [0; 128]).is_ok());
     }
 }

cdd21368090e2831c00fcaec7f81b4bac06033c2
Markus Westerlind <marwes91@gmail.com>
2019-06-10 00:31:03 +0200

    Decode multiple segments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use combine::{
     easy,
-    parser::{byte::byte, item::satisfy_map, repeat::many1},
+    parser::{
+        byte::{byte, num::be_u16},
+        item::{satisfy_map, value},
+        range::take,
+        repeat::many1,
+    },
     ParseError, Parser, RangeStream,
 };
 
+#[derive(Debug, Copy, Clone)]
 enum Marker {
     SOI,
     SOF(u8),
@@ -31,8 +37,29 @@ where
     }))
 }
 
+#[derive(Copy, Clone)]
+struct Segment<'a> {
+    marker: Marker,
+    data: &'a [u8],
+}
+
+fn segment<'a, I>() -> impl Parser<Output = Segment<'a>, Input = I>
+where
+    I: RangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    marker().then_partial(|&mut marker| match marker {
+        Marker::SOI => value(Segment { marker, data: &[] }).left(),
+        Marker::DQT => be_u16()
+            .then(|i| take(i.into()))
+            .map(move |data| Segment { marker, data })
+            .right(),
+        _ => panic!("Unhandled marker {:?}", marker),
+    })
+}
+
 pub fn decode(input: &[u8], output: &mut [u8]) -> Result<(), easy::Errors<String, String, usize>> {
-    let mut parser = many1::<Vec<_>, _>(marker());
+    let mut parser = many1::<Vec<_>, _>(segment());
     parser.easy_parse(input).map(|_| ()).map_err(|err| {
         err.map_position(|pos| pos.translate_position(input))
             .map_token(|token| format!("0x{:X}", token))

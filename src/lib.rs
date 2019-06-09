dcc7490d0126ebb060c2855db06b3707ca8158e2
Markus Westerlind <marwes91@gmail.com>
2019-06-10 01:25:49 +0200

    Accept DHT

--- src/lib.rs
+++ src/lib.rs
@@ -35,14 +35,18 @@ where
 {
     take_until_byte(0xFF) // mozjpeg skips any non marker bytes (non 0xFF)
         .skip(take_while1(|b| b == 0xFF)) // Extraenous 0xFF bytes are allowed
-        .with(satisfy_map(|b| {
-            Some(match b {
-                0xD8 => Marker::SOI,
-                0xDB => Marker::DQT,
-                0xE0...0xEF => Marker::APP(b - 0xE0),
-                _ => return None,
+        .with(
+            satisfy_map(|b| {
+                Some(match b {
+                    0xD8 => Marker::SOI,
+                    0xC4 => Marker::DHT,
+                    0xDB => Marker::DQT,
+                    0xE0...0xEF => Marker::APP(b - 0xE0),
+                    _ => return None,
+                })
             })
-        }))
+            .expected("marker"),
+        )
 }
 
 #[derive(Copy, Clone)]
@@ -58,12 +62,20 @@ where
 {
     marker().then_partial(|&mut marker| match marker {
         Marker::SOI => value(Segment { marker, data: &[] }).left(),
-        Marker::DQT | Marker::APP(_) => be_u16()
+        Marker::DHT | Marker::DQT | Marker::APP(_) => be_u16()
             .then(|quantization_table_len| take(quantization_table_len.into()))
             .map(move |data| Segment { marker, data })
             .right(),
         _ => panic!("Unhandled marker {:?}", marker),
     })
+}
+
+fn dht<'a, I>() -> impl Parser<Output = (), Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    any().map(|b| (b & 0x0F, b >> 4)).with(value(()))
 }
 
 fn dqt<'a, I>() -> impl Parser<Output = (), Input = I>
@@ -89,6 +101,7 @@ where
 {
     match segment.marker {
         Marker::SOI => Ok(()),
+        Marker::DHT => dht().parse(I::from(segment.data)).map(|_| ()),
         Marker::DQT => dqt().parse(I::from(segment.data)).map(|_| ()),
         Marker::APP_ADOBE => app_adobe().parse(I::from(segment.data)).map(|_| ()),
         _ => panic!("Unhandled segment {:?}", segment.marker),

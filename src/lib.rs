3e558eac822281ba8eab965b9ce223d3820730ed
Markus Westerlind <marwes91@gmail.com>
2019-06-10 09:52:02 +0200

    Fix the length extraction of variable length segments

--- src/lib.rs
+++ src/lib.rs
@@ -62,8 +62,11 @@ where
         satisfy_map(|b| {
             Some(match b {
                 0xD8 => Marker::SOI,
+                0xC0...0xC2 => Marker::SOF(b - 0xC0),
                 0xC4 => Marker::DHT,
                 0xDB => Marker::DQT,
+                0xDD => Marker::DRI,
+                0xDA => Marker::SOS,
                 0xD0...0xD7 => Marker::RST(b - 0xD0),
                 0xE0...0xEF => Marker::APP(b - 0xE0),
                 0xD9 => Marker::EOI,
@@ -85,14 +88,35 @@ where
     I: FullRangeStream<Item = u8, Range = &'a [u8]>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    marker().then_partial(|&mut marker| match marker {
-        Marker::SOI | Marker::RST(_) | Marker::EOI => value(Segment { marker, data: &[] }).left(),
-        Marker::DHT | Marker::DQT | Marker::APP(_) => be_u16()
-            .then(|quantization_table_len| take(quantization_table_len.into()))
+    marker().then_partial(|&mut marker| {
+        match marker {
+        Marker::SOI | Marker::RST(_) | Marker::EOI => {
+            Parser::left(value(Segment { marker, data: &[] }).left())
+        }
+        Marker::DRI => Parser::left(take(4).map(move |data| Segment { marker, data }).right()),
+        Marker::SOF(_) | Marker::DHT | Marker::DQT | Marker::SOS | Marker::APP(_) => be_u16() // TODO Check length >= 2
+            .then(|quantization_table_len| take((quantization_table_len - 2).into()))
             .map(move |data| Segment { marker, data })
             .right(),
         _ => panic!("Unhandled marker {:?}", marker),
+    }
     })
+}
+
+fn sof0<'a, I>() -> impl Parser<Output = (), Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    any().map(|b| (b & 0x0F, b >> 4)).with(value(()))
+}
+
+fn sof2<'a, I>() -> impl Parser<Output = (), Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    any().map(|b| (b & 0x0F, b >> 4)).with(value(()))
 }
 
 fn dht<'a, I>() -> impl Parser<Output = (), Input = I>
@@ -104,6 +128,22 @@ where
 }
 
 fn dqt<'a, I>() -> impl Parser<Output = (), Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    any().map(|b| (b & 0x0F, b >> 4)).with(value(()))
+}
+
+fn dri<'a, I>() -> impl Parser<Output = (), Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    any().map(|b| (b & 0x0F, b >> 4)).with(value(()))
+}
+
+fn sos<'a, I>() -> impl Parser<Output = (), Input = I>
 where
     I: FullRangeStream<Item = u8, Range = &'a [u8]>,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
@@ -126,8 +166,12 @@ where
 {
     match segment.marker {
         Marker::SOI | Marker::RST(_) | Marker::EOI => Ok(()),
+        Marker::SOF(0) => sof0().parse(I::from(segment.data)).map(|_| ()),
+        Marker::SOF(2) => sof2().parse(I::from(segment.data)).map(|_| ()),
         Marker::DHT => dht().parse(I::from(segment.data)).map(|_| ()),
         Marker::DQT => dqt().parse(I::from(segment.data)).map(|_| ()),
+        Marker::DRI => dri().parse(I::from(segment.data)).map(|_| ()),
+        Marker::SOS => sos().parse(I::from(segment.data)).map(|_| ()),
         Marker::APP_ADOBE => app_adobe().parse(I::from(segment.data)).map(|_| ()),
         _ => panic!("Unhandled segment {:?}", segment.marker),
     }

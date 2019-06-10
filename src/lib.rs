279590d92cd82cf572fdb81ec332b9da82177083
Markus Westerlind <marwes91@gmail.com>
2019-06-10 22:46:59 +0200

    Unzigzag the DQT table

--- src/lib.rs
+++ src/lib.rs
@@ -208,6 +208,12 @@ where
         .with(value(()))
 }
 
+static UNZIGZAG: [u8; 64] = [
+    0, 1, 8, 16, 9, 2, 3, 10, 17, 24, 32, 25, 18, 11, 4, 5, 12, 19, 26, 33, 40, 48, 41, 34, 27, 20,
+    13, 6, 7, 14, 21, 28, 35, 42, 49, 56, 57, 50, 43, 36, 29, 22, 15, 23, 30, 37, 44, 51, 58, 59,
+    52, 45, 38, 31, 39, 46, 53, 60, 61, 54, 47, 55, 62, 63,
+];
+
 struct DQT([u16; 64]);
 
 impl Default for DQT {
@@ -224,8 +230,8 @@ where
     where
         T: IntoIterator<Item = A>,
     {
-        for (to, from) in self.0.iter_mut().zip(iter) {
-            *to = from.into();
+        for (i, from) in (0..64).zip(iter) {
+            self.0[usize::from(UNZIGZAG[i])] = from.into();
         }
     }
 }

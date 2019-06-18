424260f5debcb5011b47a1c09ee8fadd2656154b
Markus Westerlind <marwes91@gmail.com>
2019-06-18 19:55:32 +0200

    refactor: Factor out Marker to a module

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,21 @@ use combine::{
     error::{Consumed, ParseError, StreamError},
     parser,
     parser::{
-        byte::{byte, num::be_u16, take_until_byte},
+        byte::num::be_u16,
         item::{any, eof, satisfy_map, value},
-        range::{range, take, take_while1},
-        repeat::{count_min_max, many1, sep_by1, skip_many1},
+        range::{range, take},
+        repeat::{count_min_max, many1, skip_many1},
     },
     stream::{FullRangeStream, StreamErrorFor},
     Parser,
 };
 
+use marker::{marker, Marker};
+
 mod color_conversion;
 mod huffman;
 mod idct;
+mod marker;
 mod upsampler;
 
 #[derive(Debug, PartialEq, Eq, Display)]
@@ -41,81 +44,6 @@ pub enum Error {
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum Marker {
-    SOI,
-    // SOF 0 : Baseline DCT
-    // SOF 1 : Extended sequential DCT, Huffman coding
-    // SOF 2 : Progressive DCT, Huffman coding
-    // SOF 3 : Lossless (sequential), Huffman coding
-    // SOF 9 : Extended sequential DCT, arithmetic coding
-    // SOF 10 : Progressive DCT, arithmetic coding
-    // SOF 11 : Lossless (sequential), arithmetic coding
-    SOF(u8),
-    DHT,
-    DQT,
-    DRI,
-    SOS,
-    RST(u8),
-    APP(u8),
-    COM,
-    EOI,
-}
-
-impl Marker {
-    const APP_ADOBE: Marker = Marker::APP(14);
-}
-
-fn marker<'a, I>() -> impl Parser<Output = Marker, Input = I>
-where
-    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    #[derive(Clone)]
-    pub struct Sink;
-
-    impl Default for Sink {
-        fn default() -> Self {
-            Sink
-        }
-    }
-
-    impl<A> Extend<A> for Sink {
-        fn extend<T>(&mut self, iter: T)
-        where
-            T: IntoIterator<Item = A>,
-        {
-            iter.into_iter().for_each(|_| ())
-        }
-    }
-
-    sep_by1::<Sink, _, _>(
-        (
-            take_until_byte(0xFF),      // mozjpeg skips any non marker bytes (non 0xFF)
-            take_while1(|b| b == 0xFF), // Extraenous 0xFF bytes are allowed
-        ),
-        byte(0x00).expected("stuffed zero"), // When we encounter a 0x00, we found a stuffed zero (FF/00) sequence so we search again
-    )
-    .with(
-        satisfy_map(|b| {
-            Some(match b {
-                0xD8 => Marker::SOI,
-                0xC0...0xC2 => Marker::SOF(b - 0xC0),
-                0xC4 => Marker::DHT,
-                0xDB => Marker::DQT,
-                0xDD => Marker::DRI,
-                0xDA => Marker::SOS,
-                0xD0...0xD7 => Marker::RST(b - 0xD0),
-                0xE0...0xEF => Marker::APP(b - 0xE0),
-                0xD9 => Marker::EOI,
-                0xFE => Marker::COM,
-                _ => return None,
-            })
-        })
-        .expected("marker"),
-    )
-}
 
 fn split_4_bit<'a, I>() -> impl Parser<Output = (u8, u8), Input = I>
 where
---
+++ src/marker.rs
@@ -0,0 +1,86 @@
+use combine::{
+    error::ParseError,
+    parser::{
+        byte::{byte, take_until_byte},
+        item::satisfy_map,
+        range::take_while1,
+        repeat::sep_by1,
+    },
+    stream::FullRangeStream,
+    Parser,
+};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum Marker {
+    SOI,
+    // SOF 0 : Baseline DCT
+    // SOF 1 : Extended sequential DCT, Huffman coding
+    // SOF 2 : Progressive DCT, Huffman coding
+    // SOF 3 : Lossless (sequential), Huffman coding
+    // SOF 9 : Extended sequential DCT, arithmetic coding
+    // SOF 10 : Progressive DCT, arithmetic coding
+    // SOF 11 : Lossless (sequential), arithmetic coding
+    SOF(u8),
+    DHT,
+    DQT,
+    DRI,
+    SOS,
+    RST(u8),
+    APP(u8),
+    COM,
+    EOI,
+}
+
+impl Marker {
+    pub const APP_ADOBE: Marker = Marker::APP(14);
+}
+
+pub fn marker<'a, I>() -> impl Parser<Output = Marker, Input = I>
+where
+    I: FullRangeStream<Item = u8, Range = &'a [u8]>,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    #[derive(Clone)]
+    pub struct Sink;
+
+    impl Default for Sink {
+        fn default() -> Self {
+            Sink
+        }
+    }
+
+    impl<A> Extend<A> for Sink {
+        fn extend<T>(&mut self, iter: T)
+        where
+            T: IntoIterator<Item = A>,
+        {
+            iter.into_iter().for_each(|_| ())
+        }
+    }
+
+    sep_by1::<Sink, _, _>(
+        (
+            take_until_byte(0xFF),      // mozjpeg skips any non marker bytes (non 0xFF)
+            take_while1(|b| b == 0xFF), // Extraenous 0xFF bytes are allowed
+        ),
+        byte(0x00).expected("stuffed zero"), // When we encounter a 0x00, we found a stuffed zero (FF/00) sequence so we search again
+    )
+    .with(
+        satisfy_map(|b| {
+            Some(match b {
+                0xD8 => Marker::SOI,
+                0xC0...0xC2 => Marker::SOF(b - 0xC0),
+                0xC4 => Marker::DHT,
+                0xDB => Marker::DQT,
+                0xDD => Marker::DRI,
+                0xDA => Marker::SOS,
+                0xD0...0xD7 => Marker::RST(b - 0xD0),
+                0xE0...0xEF => Marker::APP(b - 0xE0),
+                0xD9 => Marker::EOI,
+                0xFE => Marker::COM,
+                _ => return None,
+            })
+        })
+        .expected("marker"),
+    )
+}

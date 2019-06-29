05a67bf811d0a06be83f4360e5e8e86b7abc8368
Markus Westerlind <marwes91@gmail.com>
2019-06-29 11:46:49 +0200

    Add mozjpeg to compare benchmarks with

--- benches/basic.rs
+++ benches/basic.rs
@@ -10,11 +10,14 @@ fn bench_decode(b: &mut Bencher, name: &str) {
 }
 fn bench_decode_path(b: &mut Bencher, in_path: &Path, name: &str) {
     let _ = env_logger::try_init();
-    let mut decoder = Decoder::default();
     let input =
         fs::read(in_path).unwrap_or_else(|err| panic!("Can't read image {}: {}", name, err));
 
-    b.iter(|| decoder.decode(&input).unwrap());
+    b.iter(|| {
+        let mut decoder = Decoder::default();
+        let out = decoder.decode(&input).unwrap();
+        out
+    });
 }
 
 fn it_works(b: &mut Bencher) {
@@ -29,10 +32,28 @@ fn simple(b: &mut Bencher) {
     bench_decode(b, "simple");
 }
 
+fn it_works_mozjpeg(b: &mut Bencher) {
+    use mozjpeg::CompInfoExt;
+
+    let name = "img0.jpg";
+    let input = fs::read(Path::new(name))
+        .unwrap_or_else(|err| panic!("Can't read image {}: {}", name, err));
+
+    b.iter(|| {
+        let decompress = mozjpeg::Decompress::new_mem(&input).unwrap();
+        let mut started = decompress.rgb().unwrap();
+        let out = started.read_scanlines::<[u8; 3]>().unwrap();
+        assert!(started.finish_decompress());
+        out
+    })
+}
+
 fn decode_group(c: &mut Criterion) {
     c.bench(
         "it_works",
-        Benchmark::new("it_works", it_works).sample_size(10),
+        Benchmark::new("combine", it_works)
+            .with_function("mozjpeg", it_works_mozjpeg)
+            .sample_size(10),
     );
     c.bench_function("green", green);
     c.bench_function("simple", simple);
